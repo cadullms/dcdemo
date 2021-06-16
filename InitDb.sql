1b46bcd764723736d5b73553aabe3a0ccf38a1a2
cadullms <cadullms@github.com>
2021-06-16 22:05:02 +0000

    initial

---
+++ InitDb.sql
@@ -0,0 +1,11 @@
+create table if not exists Lines 
+(
+    Line VARCHAR(120)
+);
+
+DELETE FROM helloworld;
+
+INSERT INTO Lines (Line) VALUES ('I am he');
+INSERT INTO Lines (Line) VALUES ('As you are he');
+INSERT INTO Lines (Line) VALUES ('As I am he');
+INSERT INTO Lines (Line) VALUES ('And we are all together');
---
+++ get-data.py
@@ -0,0 +1,16 @@
+# So you just want to try out how to work with PostgreSQL
+# in Python and stumble upon a sample like this: https://wiki.postgresql.org/wiki/Psycopg2_Tutorial 
+
+import psycopg2
+
+try:
+    conn = psycopg2.connect("dbname='postgres' user='postgres' host='db' password='postgres'")
+except:
+    print("I am unable to connect to the database")
+
+cur = conn.cursor()
+cur.execute("""SELECT Line from Lines""")
+rows = cur.fetchall()
+print ("Show me the lyrics:")
+for row in rows:
+    print ("   ", row[0])
